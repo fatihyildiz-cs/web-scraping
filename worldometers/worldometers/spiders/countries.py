e7d1205eafe099cb01fc4d8dad0827556c145378
Fatih Yildiz <fatihyildiz.cs@gmail.com>
2021-11-05 10:12:00 +0300

    yield request to each country link

--- worldometers/worldometers/spiders/countries.py
+++ worldometers/worldometers/spiders/countries.py
@@ -4,7 +4,7 @@ import scrapy
 
 class CountriesSpider(scrapy.Spider):
     name = 'countries'
-    allowed_domains = ['www.worldometers.info/']
+    allowed_domains = ['www.worldometers.info'] # mind the extra "/" at the end to not get "filtered offsite request" error.
     start_urls = ['https://www.worldometers.info/world-population/population-by-country/']
 
     def parse(self, response):
@@ -14,7 +14,8 @@ class CountriesSpider(scrapy.Spider):
             name = country.xpath(".//text()").get()
             link = country.xpath(".//@href").get()
 
-            yield {
-                'title': name,
-                'country_link': link
-            }
+            # absolute_url = f"https://www.worldometers.info{link}"
+            absolute_url = response.urljoin(link)
+
+            # yield scrapy.Request(url=absolute_url) # this also works
+            yield response.follow(url=link)
