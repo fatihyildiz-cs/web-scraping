a4cfa9477cd2b79210f3275645bdcd69611cdf8c
Fatih Yildiz <fatihyildiz.cs@gmail.com>
2021-11-05 11:01:41 +0300

    scrape country name and gdp debt from worldpopulationreview.com

---
+++ national_debt/national_debt/spiders/gdp_debt.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class GdpDebtSpider(scrapy.Spider):
+    name = 'gdp_debt'
+    allowed_domains = ['www.worldpopulationreview.com/countries/countries-by-national-debt']
+    start_urls = ['http://www.worldpopulationreview.com/countries/countries-by-national-debt/']
+
+    def parse(self, response):
+
+        rows = response.xpath("//table/tbody/tr")
+        for row in rows:
+            
+            country_name = row.xpath(".//td[1]/a/text()").get()
+            gdp_debt = row.xpath(".//td[2]/text()").get()
+
+            yield{
+                'country_name': country_name,
+                'gdp_debt': gdp_debt
+            }
